8704a1f7ed4a888b1a11c046c815ca12d26f46f1
qjmpsial <153688119+qjmpsial@users.noreply.github.com>
2023-12-13 07:56:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('plant_classifier.hdf5')
+  model=tf.keras.models.load_model('final_model.h5')
   return model
 model=load_model()
 st.write("""
