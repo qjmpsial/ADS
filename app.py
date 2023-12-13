50920d139406519f4fcaf2ed535f6859742c6289
qjmpsial <153688119+qjmpsial@users.noreply.github.com>
2023-12-13 09:33:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,15 +47,17 @@ else:
                   'palm_tree', 'pear', 'pickup_truck', 'pine_tree', 'plain', 'plate', 'poppy', 'porcupine', 'possum', 'rabbit', 'raccoon', 'ray', 'road', 
                   'rocket', 'rose', 'sea', 'seal', 'shark', 'shrew', 'skunk', 'skyscraper', 'snail', 'snake', 'spider', 'squirrel', 'streetcar', 'sunflower', 
                   'sweet_pepper', 'table', 'tank', 'telephone', 'television', 'tiger', 'tractor', 'train', 'trout', 'tulip', 'turtle', 'wardrobe', 'whale', 'willow_tree', 'wolf', 'woman', 'worm']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    # string="OUTPUT : "+class_names[np.argmax(prediction) + result]
+    st.success(string)
 
-def get_str_label(class_name, class_names, str_labels):
-  try:
-      index = class_names.index(class_name)
-      return str_labels[index]
-  except ValueError:
-      return None  # Return None if the class name is not found in class_names
+# def get_str_label(class_name, class_names, str_labels):
+#   try:
+#       index = class_names.index(class_name)
+#       return str_labels[index]
+#   except ValueError:
+#       return None  # Return None if the class name is not found in class_names
       
-result = get_str_label(class_names[np.argmax(prediction)], class_names, str_labels)
+# result = get_str_label(class_names[np.argmax(prediction)], class_names, str_labels)
   
-string="OUTPUT : "+class_names[np.argmax(prediction) + result]
-st.success(string)
+
