aa1bebcf6a115d7a50af91da8166a1b64d29305b
qjmpsial <153688119+qjmpsial@users.noreply.github.com>
2023-12-13 08:13:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,11 @@ else:
     image=Image.open(file)
     st.image(image,use_column_width=True)
     prediction=import_and_predict(image,model)
-    class_names=['Alpinia Galanga (Rasna)','Amaranthus Viridis (Arive-Dantu)',
-                 'Artocarpus Heterophyllus (Jackfruit)',
-                 'Azadirachta Indica (Neem)','Basella Alba (Basale)',
-                 'Brassica Juncea (Indian Mustard)','Carissa Carandas (Karanda)',
-                 'Citrus Limon (Lemon)','Ficus Auriculata (Roxburgh fig)',
-                 'Ficus Religiosa (Peepal Tree)','Hibiscus Rosa-sinensis',
-                 'Jasminum (Jasmine)','Mangifera Indica (Mango)',
-                 'Mentha (Mint)','Moringa Oleifera (Drumstick)',
-                 'Muntingia Calabura (Jamaica Cherry-Gasagase)',
-                 'Murraya Koenigii (Curry)', 'Nerium Oleander (Oleander)',
-                 'Nyctanthes Arbor-tristis (Parijata)','Ocimum Tenuiflorum (Tulsi)',
-                 'Piper Betle (Betel)','Plectranthus Amboinicus (Mexican Mint)',
-                 'Pongamia Pinnata (Indian Beech)','Psidium Guajava (Guava)',
-                 'Punica Granatum (Pomegranate)','Santalum Album (Sandalwood)',
-                 'Syzygium Cumini (Jamun)','Syzygium Jambos (Rose Apple)',
-                 'Tabernaemontana Divaricata (Crape Jasmine)',
-                 'Trigonella Foenum-graecum (Fenugreek)']
+    class_names=[0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16,
+       17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+       34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+       51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+       68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84,
+       85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
