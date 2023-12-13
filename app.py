19f79f855e6bcc18b7adba6adef436b79f0389a7
qjmpsial <153688119+qjmpsial@users.noreply.github.com>
2023-12-13 08:19:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -14,13 +14,22 @@ file=st.file_uploader("Choose plant photo from computer",type=["jpg","png"])
 import cv2
 from PIL import Image,ImageOps
 import numpy as np
-def import_and_predict(image_data,model):
-    size=(64,64)
-    image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
-    img=np.asarray(image)
-    img_reshape=img[np.newaxis,...]
-    prediction=model.predict(img_reshape)
+
+# def import_and_predict(image_data,model):
+#     size=(64,64)
+#     image=ImageOps.fit(image_data,size,Image.ANTIALIAS)
+#     img=np.asarray(image)
+#     img_reshape=img[np.newaxis,...]
+#     prediction=model.predict(img_reshape)
+#     return prediction
+def import_and_predict(image_data, model):
+    size = (64, 64)
+    image = image_data.resize(size, Image.ANTIALIAS)  # <-- Modified line
+    img = np.asarray(image)
+    img_reshape = img[np.newaxis, ...]
+    prediction = model.predict(img_reshape)
     return prediction
+
 if file is None:
     st.text("Please upload an image file")
 else:
